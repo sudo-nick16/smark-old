408ac1fddd23e5104b340aa980a26c0c675c7ea2
Nikit <singhnikit16@gmail.com>
2021-07-11 18:23:34 +0530

    trying sessionStorage

--- sharehandler.js
+++ sharehandler.js
@@ -1,3 +1,11 @@
-const url = window.location.hash
-window.close()
-chrome.runtime.sendMessage(url)
+console.log(window.location)
+if(sessionStorage.getItem("/share/")){
+    const url = sessionStorage.getItem("/share/");
+    sessionStorage.removeItem("/share");
+    window.close()
+    chrome.runtime.sendMessage(url)
+}
+
+// const url = window.location.hash
+// window.close()
+// chrome.runtime.sendMessage(url)
